a6d66bc0cb4ec5c2bfb7ef0ad5d6b2b1b15b79af
SanchoFdz <70403060+SanchoFdz@users.noreply.github.com>
2022-06-21 14:47:43 -0500

    Se agregó la función para calcular ángulos y distancias

--- Codigos/Jugadas/normalizacion.py
+++ Codigos/Jugadas/normalizacion.py
@@ -1,3 +1,7 @@
+import pandas as pd
+import math 
+import numpy as np
+
 def normalizarDimensiones(df,tam=[110,70]):
     '''
     Esta función normaliza los valores de las columnas de coordenadas y regresa un
@@ -53,3 +57,49 @@ def normalizarDirecciones(df,tam=[110,75]):
 
 #Test Case
 dfx2 = normalizarDirecciones(dfx)
+
+def distanciaGol(x,y,team=0,tam=[110,75],return_angle=True):
+    '''
+    Funcion para calcular la distancia y angulo entre un punto (x,y)
+    y la porteria que ataca el equipo team.
+    
+    Puede o no regresar el angulo segun lo desee el usuario.
+
+    Args:
+        x (NUMERIC): coordenada x.
+        y (NUMERIC): coordenada y.
+        team (NUMERIC, optional): Equipo a estudiar. Si es 0 el equipo que ataca
+                                 la porteria en (0,maxY/2) en el periodo 1H. Si es 1 el que ataca
+                                 la porteria en (maxX, maxY/2). Defaults to 0.
+        tam (LIST, optional): Dimensiones de la cancha [maxX,maxY]. Defaults to [110,75].
+        return_angle (BOOLEAN, optional): Condición para regresar o no el angulo.
+                                          Defaults to True.
+
+    Returns:
+        dist (NUMERIC): distancia entre (x,y) y la porteria que se ataca.
+        ang (NUMERIC, optional): angulo en radianes entre el punto (x,y) y 
+
+    '''
+    
+    if team:
+        dist = math.sqrt((tam[0]-x)**(2) + (tam[1]/2-y)**(2))
+    else:
+        dist = math.sqrt((0-x)**(2) + (tam[1]/2-y)**(2))
+    
+    if return_angle:
+        v2 = [0,tam[0]]
+        if team:
+            v1 = [tam[0]-x,tam[1]/2-y]
+        else:
+            v1 = [0-x,tam[1]/2-y]
+            
+        u1 = v1 / np.linalg.norm(v1)
+        u2 = v2 / np.linalg.norm(v2)
+        ang = np.arccos(np.dot(u1, u2))
+        return (dist,ang)
+    else:
+        return (dist,None)
+    
+#Test Case
+#x = 70, y = 50
+distancia,angulo = distanciaGol(70,50,1)
