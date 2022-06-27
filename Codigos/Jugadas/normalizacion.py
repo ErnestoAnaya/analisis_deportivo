290df4b37d1b62207c455389f8893763c9444ce2
ErnestoAnaya <eanayaol@itam.mx>
2022-06-27 00:12:08 -0500

    anadir voltear coordenadas

--- Codigos/Jugadas/normalizacion.py
+++ Codigos/Jugadas/normalizacion.py
@@ -24,7 +24,7 @@ def normalizarDimensiones(df,tam=[110,70]):
 
 
 #Test Case: sobre el DF de jugadas
-dfx = normalizarDimensiones(dfmid)
+#dfx = normalizarDimensiones(dfmid)
 
 def normalizarDirecciones(df,tam=[110,75]):
     '''
@@ -56,7 +56,7 @@ def normalizarDirecciones(df,tam=[110,75]):
     return df1
 
 #Test Case
-dfx2 = normalizarDirecciones(dfx)
+#dfx2 = normalizarDirecciones(dfx)
 
 def distanciaGol(x,y,team=0,tam=[110,75],return_angle=True):
     '''
@@ -103,3 +103,70 @@ def distanciaGol(x,y,team=0,tam=[110,75],return_angle=True):
 #Test Case
 #x = 70, y = 50
 distancia,angulo = distanciaGol(70,50,1)
+
+def crear_is_home(df):
+    '''
+    Agregar al df una columna is_home, establece un equipo de un partido como referencia,
+    notar que el valor es 0 o otro entero. NORTA: podría ser 0 o 1.
+    
+    La función se usa en cambiar_coordenadas()
+    
+    Returns:
+        df: df con la columna extra de is_home
+    '''
+    
+    #df2 va a ser un df que por cada partido, tiene columnas separadas para cada equipo del partido
+    df2 = df[['matchId','teamId','x_inicio']]
+    
+    df2 = df.groupby(['matchId','teamId']).size().reset_index().rename(columns={0:'count'})
+    df2['team2'] = df2['teamId'].shift(-1)
+    df2['match2'] = df2['matchId'].shift(-1)
+    df2['same_match'] = df2['matchId']-df2['match2']
+    
+    df2.loc[df2.same_match == -1, "team2"] = df2.teamId
+    df2.dropna(subset = ['same_match'], inplace=True)
+    
+    
+    df2 = df2[df2['teamId'] != df2['team2']]
+    df2['team1'] = df2['teamId']
+    df2 = df2.drop(columns=['teamId'])
+    
+    #join del df con df2, así ya podemos fijar un equipo como "local"
+    # la nueva columna permite afectar eventos de un solo equipo por cada partido.
+    result = pd.merge(df,df2, how="left", on="matchId")
+    result = result.drop(columns=['same_match'])
+    result['is_home'] = result['teamId']-result['team1'] 
+        
+    return result
+
+def cambiar_coordenadas(df):
+    '''
+    Recibe un df de eventos. Agrega la columna is_home y luego voltea todos los 
+    eventos de un equipo para cada partido. Se mantienen las coordenadas originales 
+    
+    NOTA: esta versión si voltea las coordenadas, pero el equipo que se toma 
+    como referencia es arbitrario.
+    
+    LIMPIAR CODIGO
+    
+    '''
+    norm1 = crear_is_home(df)
+    
+    #declarar las nuevas columnas
+    norm1['x_inic_norm']  = df.x_inicio
+    norm1['x_fin_norm'] = df.x_fin
+    
+    norm1['x_inic_norm'] = df.loc[df.is_home != 0, "x_inicio"] = 100-df.x_inicio
+    norm1['x_fin_norm'] = df.loc[df.is_home != 0, "x_fin"] = 100-df.x_fin
+    
+    
+    ##Aun hay que checar que con diferentes datos, las cooerdenadas se voltean 
+    # de forma adecuada
+    
+    #para los del mundial tenía que correr el siguiente bloque 
+    
+    #ojo: luego hay que cambiar todo lo de '2H'. Si no estárían al revés. en 1h todos de izq a der y en 2h de der a izq
+    #norm1['x_inic_norm'] = result.loc[result.matchPeriod != '1H', "x_inic_norm"] = 100-result.x_inicio
+    #norm1['x_fin_norm'] = result.loc[result.matchPeriod != '1H', "x_fin"] = 100-result.x_fin
+    
+    return norm1
